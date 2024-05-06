6cee99605a09ef714aa569332628e1ec97529396
kaique-psantos <kaique212pereira@gmail.com>
2024-05-06 20:15:15 -0300

    Adicionado a função de criação de usuário com firebase

--- src/screens/CreateUser/index.js
+++ src/screens/CreateUser/index.js
@@ -1,9 +1,45 @@
+import React, { useState } from 'react'
+import { firebase } from '../../services/firebaseConfig'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
 import styles from './style'
 
-export default function CreateUser() {
-    let errorCreateUser = null
+export default function CreateUser({navigation}) {
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorCreateUser, setErrorCreateUser] = useState(null)
+
+
+    function validate() {
+        if (nome == "") {
+            setErrorCreateUser("Informe o seu nome")
+        } else if (email == "") {
+            setErrorCreateUser("Informe o seu e-mail")
+        } else if (password == "") {
+            setErrorCreateUser("Infome uma password")
+        } else {
+            // Caso não haja erro, chama a função de criação de usuário
+            setErrorCreateUser(null)
+            createUser();
+        }
+    }
+
+    const createUser = () => {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                navigation.navigate('Tabs')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // Mostro o erro do usuário
+                setErrorCreateUser(errorMessage)
+            });
+    }
 
     return (
         <View style={styles.container}>
@@ -14,21 +50,28 @@ export default function CreateUser() {
             <TextInput
                 style={styles.input}
                 placeholder='Nome'
+                value={nome}
+                onChangeText={setNome}
             />
 
             <TextInput
                 style={styles.input}
                 placeholder='E-mail'
+                value={email}
+                onChangeText={setEmail}
             />
 
             <TextInput
                 style={styles.input}
                 secureTextEntry={true}
-                placeholder='Senha'
+                placeholder='password'
+                value={password}
+                onChangeText={setPassword}
             />
 
             <TouchableOpacity
                 style={styles.button}
+                onPress={validate}
             >
                 <Text style={styles.textButton}>Criar usuário</Text>
             </TouchableOpacity>
---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxYvyZ0LvYgKpYpB9pYNbmReEsnNVi1Ds",
+  authDomain: "pra-fazer-app-212.firebaseapp.com",
+  projectId: "pra-fazer-app-212",
+  storageBucket: "pra-fazer-app-212.appspot.com",
+  messagingSenderId: "992638431492",
+  appId: "1:992638431492:web:1bf566a2bb6aebc18ce051"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
