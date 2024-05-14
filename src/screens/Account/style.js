1cec971f1fbfe3639fd9c786307c3f92758bc9d4
kaique-psantos <kaique212pereira@gmail.com>
2024-05-14 09:57:08 -0300

    Adicionado validação de login e modificações na tela Account

---
+++ src/screens/Account/style.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    container: {
+        padding: 30,
+        alignItems: 'center'
+
+    },
+
+    title: {
+        fontWeight: 'bold',
+        marginBottom: 20,
+        fontSize: 18
+
+    },
+
+    info: {
+        marginBottom: 20,
+        fontSize: 18
+    },
+
+    button: {
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#F60',
+        borderRadius: 10,
+        width: '100%'
+    },
+
+    textButton: {
+        fontSize: 18,
+        textAlign: 'center',
+        color: 'F60'
+    }
+})
+
+export default styles
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,29 +1,64 @@
+import { Firebase } from '../../services/firebaseConfig'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './style'
 
 export default function Login({ navigation }) {
-    let errorLogin = null
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorLogin, setErrorLogin] = useState(null)
+
+    function validate() {
+        if (email == "") {
+            setErrorLogin("Informe um email")
+        } else if (password == "") {
+            setErrorLogin("Informe a senha")
+        } else {
+            setErrorLogin(null)
+            login()
+        }
+    }
+
+    function login() {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                navigation.navigate('Tabs')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
 
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require('../../../assets/logo_pra_fazer.png')} />
 
-            { errorLogin != null && (
-                <Text style={styles.alert}>{ errorLogin }</Text>
+            {errorLogin != null && (
+                <Text style={styles.alert}>{errorLogin}</Text>
             )}
 
             <TextInput
                 style={styles.input}
                 placeholder='E-mail'
+                value={email}
+                onChangeText={setEmail}
             />
 
             <TextInput
                 style={styles.input}
                 placeholder='Senha'
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}
             />
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+                onPress={validate}>
                 <Text style={styles.textButton}>Entrar</Text>
             </TouchableOpacity>
 
